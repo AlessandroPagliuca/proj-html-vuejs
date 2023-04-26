1b53f278511df196f16044aa9d168bb17ce79aaf
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-26 13:39:43 +0200

    Upgraded Advertising banner

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,6 +1,19 @@
 <template>
     <header>
        <UpdatesComp />
+       <!--Advertising banner-->
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center py-3">
+                <div class="img-anime ">
+                    <img src="/image/anime-logo.webp" alt="anime-logo">
+                </div>
+
+                <div class="img-banner">
+                    <img src="/image/header-banner.webp" alt="">
+                </div>
+            </div>
+        </div>
+      
     </header>
 </template>
 
@@ -15,6 +28,28 @@ import UpdatesComp from './UpdatesComp.vue';
 </script>
 
 <style lang="scss" scoped>
+
+    .img-anime{
+        width: 160px;
+        height: 50px;
+
+        img{
+            width: 100%;
+            height: 100%;
+        }
+    }
+    .img-banner{
+        width: 420px;
+        height: 70px;
+
+        img{
+            width: 100%;
+            height: 100%;
+            border-radius: 5px;
+
+        }
+    }
+
    
 
 
