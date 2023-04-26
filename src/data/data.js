a51eac483caf496808583c1dfec7e4b88f16c655
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-26 15:11:29 +0200

    Upgraded dataLinks

---
+++ src/data/data.js
@@ -0,0 +1,86 @@
+const dataLinks = [
+    {
+        title: 'get started',
+        links:[
+            {
+                link:'resources',
+            },
+            {
+                link:'tutorials',
+            },
+            {
+                link:'examples',
+            },
+            {
+                link:'docs',
+            },
+        ]
+    },
+
+    {
+        title: 'about',
+        links:[
+            {
+                link:'stories',
+            },
+
+            {
+                link:'community',
+            },
+
+            {
+                link:'blog',
+            },
+
+            {
+                link:'brand assets',
+            },
+        ]
+    },
+
+    {
+        title: 'features',
+        links:[
+            {
+                link:'overview',
+            },
+
+            {
+                link:'design',
+            },
+
+            {
+                link:'code',
+            },
+
+            {
+                link:'collaborate',
+            },
+            
+            
+            
+            
+        ]
+    },
+
+    {
+        title: 'quick links',
+        links:[
+            {
+                link:'stories',
+            },
+
+            {
+                link:'community',
+            },
+
+            {
+                link:'blog',
+            },
+
+            {
+                link:'brand assets',
+            },
+        ]
+    },
+]
