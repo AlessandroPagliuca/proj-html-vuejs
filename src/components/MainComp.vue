ce52a5fa961fb790d88ff53018361f761e0db620
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-26 21:33:37 +0200

    Added SecondCarousel in template

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,19 +1,25 @@
 <template>
     <main>
         <CarouselHorizontal />
+        <SecondCarousel />
     </main>
 </template>
 
 <script>
 import CarouselHorizontal from './CarouselHorizontal.vue';
+import SecondCarousel from './SecondCarousel.vue';
     export default {
         name: 'MainComp',
         components:{
             CarouselHorizontal,
+            SecondCarousel,
         }
     }
 </script>
 
 <style lang="scss" scoped>
+    main{
+        padding-top: 179px;
+    }
 
 </style>
---
+++ src/components/SecondCarousel.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="container d-flex justify-content-center py-5">
+
+        <div class="d-flex flex-column align-items-center">
+            <div class="card me-2" style="width: 14rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">prova</p>
+                </div>
+            </div>
+
+            <div class="card me-2 mt-2" style="width: 14rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">prova</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex ">
+            <div class="card" style="width: 28rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">prova</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex flex-column align-items-center">
+            <div class="card ms-2" style="width: 14rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">prova</p>
+                </div>
+            </div>
+
+            <div class="card ms-2 mt-2" style="width: 14rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">prova</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import { secondCarousels } from '../data/data';
+    export default {
+        name: 'SecondCarousel',
+        data(){
+            return{
+                carousels: secondCarousels,
+                count: 0,
+            }
+        },
+        methods:{
+            filterLeftCol(){
+                this.carousels[count].id;
+                console.log(this.carousels[count].id);
+
+            }
+        },
+        mounted(){
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
