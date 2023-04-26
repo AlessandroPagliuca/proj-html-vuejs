72eaae94bcdc441a603f160f74ecc9f6bb6fab90
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-26 17:10:42 +0200

    Upgraded dataCarousel

--- src/data/data.js
+++ src/data/data.js
@@ -86,6 +86,208 @@ const dataLinks = [
             },
         ]
     },
+];
+
+const dataCarousel = [
+    {
+        img: '/healthy-foods.webp',
+        data: 'The Best Healthy Foods in 2022',
+        smallDate: 'December 26, 2022',
+        userIcon: '',
+        dateIcon: '',
+        text: '',
+        tags:[
+            {
+                tag: 'food',
+            }
+        ]
+    },
+
+    {
+        img: '/winter.webp',
+        data: 'The Best Winter Outfits',
+        smallDate: 'December 26, 2022',
+        userIcon: '',
+        dateIcon: '',
+        text: '',
+        tags:[
+            {
+                tag: 'fashion',
+            }
+        ]
+    },
+
+    {
+        img: '/photographers-mistakes.webp',
+        data: 'Beginner Photographer\'s Mistakes',
+        smallDate: 'December 26, 2022',
+        userIcon: '',
+        dateIcon: '',
+        text: '',
+        tags:[
+            {
+                tag: 'fashion',
+            }
+        ]
+    },
+
+    {
+        img: '/ideas-anime.webp',
+        data: 'Live Ideas You Might Be Anime',
+        smallDate: 'December 26, 2022',
+        userIcon: '',
+        dateIcon: '',
+        text: '',
+        tags:[
+            {
+                tag: 'culture',
+            },
+
+            {
+                tag: 'stories',
+            }
+        ]
+    },
+
+    {
+        img: '/ideas-anime.webp',
+        data: 'Live Ideas You Might Be Anime',
+        smallDate: 'December 26, 2022',
+        userIcon: '',
+        dateIcon: '',
+        text: '',
+        tags:[
+            {
+                tag: 'culture',
+            },
+
+            {
+                tag: 'stories',
+            }
+        ]
+    },
+
+    {
+        img: '/anime-fashion.webp',
+        data: 'Fashion Trend Now A Days',
+        smallDate: 'December 25, 2022',
+        userIcon: 'fa-solid fa-user',
+        dateIcon: 'fa-solid fa-',
+        text: 'demo',
+        tags:[
+            {
+                tag: 'Fashion',
+            },
+
+            {
+                tag: 'Lifestyle',
+            }
+        ]
+    },
+
+    {
+        img: '/travel-alone-300x200.webp',
+        data: 'Travelling Alone Is Awesome',
+        smallDate: 'December 26, 2022',
+        userIcon: 'fa-solid fa-user',
+        dateIcon: 'fa-solid fa-',
+        text: 'demo',
+        tags:[
+            {
+                tag: 'Stories',
+            },
+
+            {
+                tag: 'Travel',
+            }
+        ]
+    },
+
+    {
+        img: '/best-places.webp',
+        data: 'Places For A Road Trip',
+        smallDate: 'December 25, 2022',
+        userIcon: 'fa-solid fa-user',
+        dateIcon: 'fa-solid fa-',
+        text: 'demo',
+        tags:[
+            {
+                tag: 'Stories',
+            },
+
+            {
+                tag: 'Travel',
+            }
+        ]
+    },
+
+    {
+        img: '/best-places.webp',
+        data: 'Places For A Road Trip',
+        smallDate: 'December 25, 2022',
+        userIcon: 'fa-solid fa-user',
+        dateIcon: 'fa-solid fa-',
+        text: 'demo',
+        tags:[
+            {
+                tag: 'Stories',
+            },
+
+            {
+                tag: 'Travel',
+            }
+        ]
+    },
+
+    {
+        img: '/music-love.webp',
+        data: 'Music The Love Of My Life',
+        smallDate: 'December 25, 2022',
+        userIcon: 'fa-solid fa-user',
+        dateIcon: 'fa-solid fa-',
+        text: 'demo',
+        tags:[
+            {
+                tag: 'Culture',
+            },
+
+            {
+                tag: 'Lifestyle',
+            }
+        ]
+    },
+
+    {
+        img: '/visit-france.webp',
+        data: 'Reasons To Visit France',
+        smallDate: 'December 26, 2022',
+        userIcon: 'fa-solid fa-user',
+        dateIcon: 'fa-solid fa-',
+        text: 'demo',
+        tags:[
+            {
+                tag: 'Lifestyle',
+            },
+
+            {
+                tag: 'Travel',
+            }
+        ]
+    },
+
+    {
+        img: '/success-story.webp',
+        data: 'The Best Success Stories',
+        smallDate: 'December 25, 2022',
+        userIcon: 'fa-solid fa-user',
+        dateIcon: 'fa-solid fa-',
+        text: 'demo',
+        tags:[
+            {
+                tag: 'Culture',
+            },
+        ]
+    },
 ]
 
-export{dataLinks};
+export{dataLinks, dataCarousel};
