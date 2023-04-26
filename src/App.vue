cb7f74090986effddd5fd9c336660169433f6799
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-26 11:46:31 +0200

    Upgraded macrostructure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  
+  <HeaderComp />
+  <MainComp />
+  <FooterComp />
 </template>
 
 <script>
+import HeaderComp from './components/HeaderComp.vue';
+import MainComp from './components/MainComp.vue';
+import FooterComp from './components/FooterComp.vue';
   export default {
-    
+    name: 'App',
+    components:{
+      HeaderComp,
+      MainComp,
+      FooterComp,
+
+    }
   }
 </script>
 
