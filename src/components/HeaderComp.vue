b7ff8fc234dac391e9f705732a3a4addb0be73f1
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-26 13:17:15 +0200

    Upgraded UpdatesComp

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,15 +1,21 @@
 <template>
     <header>
-        
+       <UpdatesComp />
     </header>
 </template>
 
 <script>
+import UpdatesComp from './UpdatesComp.vue';
     export default {
-        
+        name: 'HeaderComp',
+        components:{
+            UpdatesComp,
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+   
+
 
 </style>
---
+++ src/components/UpdatesComp.vue
@@ -0,0 +1,87 @@
+<template>
+    <!--template della barra dove verrà visualizzato il NEW UPDATES con le icone dei social network-->
+     <div class="bg-updates">
+            <div class="container">
+                <div class="d-flex justify-content-between align-items-center box">
+                    <div class="box-sx d-flex align-items-center">
+                        <div class="box-sx bg-secondary d-flex align-items-center px-2">
+                            <span class="text-uppercase text-white fw-semibold ">new updates</span>
+                        </div>
+                        
+                        <div class="box-img">
+                            <img src="/image/channels4_profile.jpg" alt="">
+                        </div>
+
+                        <span class="text-white text-uppercase p-1">{{ getTime() }} the_</span>
+                    </div>
+
+                    <div class="box-dx d-flex align-items-center text-white">
+                        <div class="px-4">
+                            <i class="fa-solid fa-chevron-left p-3"></i>
+                            <i class="fa-solid fa-chevron-right p-3"></i>
+                        </div>
+
+                        <div class="">
+                            <i class="fa-brands fa-facebook p-1"></i>
+                            <i class="fa-brands fa-twitter p-1"></i>
+                            <i class="fa-brands fa-instagram p-1"></i>
+                            <i class="fa-brands fa-youtube p-1"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </div>
+</template>
+
+<script>
+    export default {
+        name: 'UpDatesComp',
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+            getTime(){
+                const d = new Date();
+                const hour = d.getHours();
+                const minutes = d.getMinutes();
+                let time = hour + ':' + minutes;
+                console.log(time);
+                return time;
+            }
+        },
+        mounted(){
+            this.getTime();
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+     .bg-updates{
+        background-color: #BF1D2E;
+        width: 100%;
+    }
+    .box{
+        height: 35px;
+    }
+    .box-sx, .box-dx{
+        height: 100%;
+    }
+    .box-img{
+        width: 35px;
+        height: 35px;
+
+        img{
+            width: 100%;
+            height: 100%;
+        }
+    }
+    .box-dx{
+
+        i{
+            cursor: pointer;
+        }
+    }
+
+</style>
