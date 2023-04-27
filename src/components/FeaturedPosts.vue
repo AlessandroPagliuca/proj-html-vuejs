0c6e9b94572aa1e36e811736c9ef3f3430f721e0
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-27 15:33:05 +0200

    Upgraded template

---
+++ src/components/FeaturedPosts.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <h4 class="text-uppercase">featured posts</h4>
+        <div class="d-flex justify-content-center align-items-center"></div>
+    </section>
+</template>
+
+<script>
+import {data} from '../data/data';
+    export default {
+        name: 'FeaturedPosts',
+        data(){
+            return{
+                data
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
