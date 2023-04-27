77ae183566d6f1c873839903ba9d14672bce2d0a
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-27 12:08:06 +0200

    Fixed size cards

--- src/components/SecondCarousel.vue
+++ src/components/SecondCarousel.vue
@@ -2,7 +2,7 @@
     <div class="container d-flex justify-content-center py-5">
 
         <div class="d-flex flex-column align-items-center">
-            <div class="card me-2" style="width: 14rem;">
+            <div class="card me-2" style="width: 20rem;">
                 <img class="card-img-top" src="/image/anime-fashion.webp" alt="Card image cap">
                 <div class="card-body">
                     <span>
@@ -15,9 +15,13 @@
                     </span>
                     <p class="card-text">Fashion Trend Now A Days</p>
                 </div>
+                <div class="box-badge">
+                    <span class="badge bg-light text-dark">Fashion</span>
+                    <span class="badge bg-light text-dark">Lifestyle</span>
+                </div>  
             </div>
 
-            <div class="card me-2 mt-2" style="width: 14rem;">
+            <div class="card me-2 mt-2" style="width: 20rem;">
                 <img class="card-img-top" src="/image/travel-alone-300x200.webp" alt="Card image cap">
                 <div class="card-body">
                     <span>
@@ -30,11 +34,15 @@
                     </span>
                     <p class="card-text">Travelling Alone Is Awesome</p>
                 </div>
+                <div class="box-badge">
+                    <span class="badge bg-light text-dark">Stories</span>
+                    <span class="badge bg-light text-dark">Travel</span>
+                </div>
             </div>
         </div>
 
         <div class="d-flex ">
-            <div class="card" style="width: 28rem;">
+            <div class="card" style="width: 40rem;">
                 <img class="card-img-top" src="/image//best-places.webp" alt="Card image cap">
                 <div class="card-body">
                     <span>
@@ -47,11 +55,16 @@
                     </span>
                     <p class="card-text">Places For A Road Trip</p>
                 </div>
+                <div class="box-badge">
+                    <span class="badge bg-light text-dark">Lifestyle</span>
+                    <span class="badge bg-light text-dark">Stories</span>
+                    <span class="badge bg-light text-dark">Travel</span>
+                </div>
             </div>
         </div>
 
         <div class="d-flex flex-column align-items-center">
-            <div class="card ms-2 fw-semibold" style="width: 14rem;">
+            <div class="card ms-2 fw-semibold" style="width: 20rem;">
                 <img class="card-img-top" src="/image/music-love.webp" alt="Card image cap">
                 <div class="card-body">
                     <span>
@@ -64,9 +77,13 @@
                     </span>
                     <p class="card-text">Music The Love Of My Life</p>
                 </div>
+                <div class="box-badge">
+                    <span class="badge bg-light text-dark">Culture</span>
+                    <span class="badge bg-light text-dark">Lifestyle</span>
+                </div>
             </div>
 
-            <div class="card ms-2 mt-2" style="width: 14rem;">
+            <div class="card ms-2 mt-2" style="width: 20rem;">
                 <img class="card-img-top" src="/image/visit-france.webp" alt="Card image cap">
                 <div class="card-body">
                     <span>
@@ -78,6 +95,10 @@
                         <span>December 26, 2022</span>
                     </span>
                     <p class="card-text">Reasons To Visit France</p>
+                </div>
+                <div class="box-badge">
+                    <span class="badge bg-light text-dark">Lifestyle</span>
+                    <span class="badge bg-light text-dark">Travel</span>
                 </div>
             </div>
         </div>
@@ -110,7 +131,7 @@ import { data } from '../data/data';
         display: flex;
         justify-content: end;
         align-items: center;
-        font-size: 12px;
+        font-size: 13px;
         text-align: center;
         background-color: black;
         
@@ -128,6 +149,20 @@ import { data } from '../data/data';
                 font-weight: bold;
             }
            
+        }
+        .box-badge{
+            position: absolute;
+            top: 10px;
+
+            .badge{
+                margin: 5px;
+                font-size: 13px;
+
+                &:hover{
+                    color:#BF1D2E !important;
+                    cursor: pointer;
+                }
+            }
         }
     }
 
