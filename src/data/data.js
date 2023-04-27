6ea5c01185824235bc80d2bdcb9e48fe6834a2e5
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-27 10:26:57 +0200

    Upgraded data

--- src/data/data.js
+++ src/data/data.js
@@ -1,271 +1,274 @@
-const dataLinks = [
-    {
-        title: 'get started',
-        links:[
-            {
-                link:'resources',
-            },
+import {reactive} from 'vue';
+export const data = reactive({
+    dataLinks:[
+        {
+            title: 'get started',
+            links:[
+                {
+                    link:'resources',
+                },
+    
+                {
+                    link:'tutorials',
+                },
+    
+                {
+                    link:'examples',
+                },
+    
+                {
+                    link:'docs',
+                },
+            ]
+        },
+    
+        {
+            title: 'about',
+            links:[
+                {
+                    link:'stories',
+                },
+    
+                {
+                    link:'community',
+                },
+    
+                {
+                    link:'blog',
+                },
+    
+                {
+                    link:'brand assets',
+                },
+            ]
+        },
+    
+        {
+            title: 'features',
+            links:[
+                {
+                    link:'overview',
+                },
+    
+                {
+                    link:'design',
+                },
+    
+                {
+                    link:'code',
+                },
+    
+                {
+                    link:'collaborate',
+                },
+                
+                
+                
+                
+            ]
+        },
+    
+        {
+            title: 'quick links',
+            links:[
+                {
+                    link:'stories',
+                },
+    
+                {
+                    link:'community',
+                },
+    
+                {
+                    link:'blog',
+                },
+    
+                {
+                    link:'brand assets',
+                },
+            ]
+        },
+    ],
+    
+    dataCarousels:[
+        {
+            id: 1,
+            img: '/healthy-foods.webp',
+            data: 'The Best Healthy Foods in 2022',
+            smallDate: 'December 26, 2022',
+            userIcon: '',
+            dateIcon: '',
+            text: '',
+            tags:[
+                {
+                    tag: 'food',
+                }
+            ]
+        },
+    
+        {
+            id: 2,
+            img: '/winter.webp',
+            data: 'The Best Winter Outfits',
+            smallDate: 'December 26, 2022',
+            userIcon: '',
+            dateIcon: '',
+            text: '',
+            tags:[
+                {
+                    tag: 'fashion',
+                }
+            ]
+        },
+    
+        {
+            id: 3,
+            img: '/photographers-mistakes.webp',
+            data: 'Beginner Photographer\'s Mistakes',
+            smallDate: 'December 26, 2022',
+            userIcon: '',
+            dateIcon: '',
+            text: '',
+            tags:[
+                {
+                    tag: 'fashion',
+                }
+            ]
+        },
+    
+        {
+            id: 4,
+            img: '/ideas-anime.webp',
+            data: 'Live Ideas You Might Be Anime',
+            smallDate: 'December 26, 2022',
+            userIcon: '',
+            dateIcon: '',
+            text: '',
+            tags:[
+                {
+                    tag: 'culture',
+                },
+    
+                {
+                    tag: 'stories',
+                }
+            ]
+        },
+    
+    ],
+    
+    secondCarousels:[
+        {
+            id: 6,
+            img: '/anime-fashion.webp',
+            data: 'Fashion Trend Now A Days',
+            smallDate: 'December 25, 2022',
+            userIcon: 'fa-solid fa-user',
+            dateIcon: 'fa-solid fa-calendar-days',
+            text: 'demo',
+            tags:[
+                {
+                    tag: 'Fashion',
+                },
+    
+                {
+                    tag: 'Lifestyle',
+                }
+            ]
+        },
+    
+        {
+            id: 7,
+            img: '/travel-alone-300x200.webp',
+            data: 'Travelling Alone Is Awesome',
+            smallDate: 'December 26, 2022',
+            userIcon: 'fa-solid fa-user',
+            dateIcon: 'fa-solid fa-calendar-days',
+            text: 'demo',
+            tags:[
+                {
+                    tag: 'Stories',
+                },
+    
+                {
+                    tag: 'Travel',
+                }
+            ]
+        },
+    
+        {
+            id: 8,
+            img: '/best-places.webp',
+            data: 'Places For A Road Trip',
+            smallDate: 'December 25, 2022',
+            userIcon: 'fa-solid fa-user',
+            dateIcon: 'fa-solid fa-calendar-days',
+            text: 'demo',
+            tags:[
+                {
+                    tag: 'Stories',
+                },
+    
+                {
+                    tag: 'Travel',
+                }
+            ]
+        },
+    
+        {
+            id: 9,
+            img: '/music-love.webp',
+            data: 'Music The Love Of My Life',
+            smallDate: 'December 25, 2022',
+            userIcon: 'fa-solid fa-user',
+            dateIcon: 'fa-solid fa-calendar-days',
+            text: 'demo',
+            tags:[
+                {
+                    tag: 'Culture',
+                },
+    
+                {
+                    tag: 'Lifestyle',
+                }
+            ]
+        },
+    
+        {
+            id: 10,
+            img: '/visit-france.webp',
+            data: 'Reasons To Visit France',
+            smallDate: 'December 26, 2022',
+            userIcon: 'fa-solid fa-user',
+            dateIcon: 'fa-solid fa-calendar-days',
+            text: 'demo',
+            tags:[
+                {
+                    tag: 'Lifestyle',
+                },
+    
+                {
+                    tag: 'Travel',
+                }
+            ]
+        },
+    
+        {
+            id: 11,
+            img: '/success-story.webp',
+            data: 'The Best Success Stories',
+            smallDate: 'December 25, 2022',
+            userIcon: 'fa-solid fa-user',
+            dateIcon: 'fa-solid fa-calendar-days',
+            text: 'demo',
+            tags:[
+                {
+                    tag: 'Culture',
+                },
+            ]
+        },
+    
+    ]
+});
 
-            {
-                link:'tutorials',
-            },
 
-            {
-                link:'examples',
-            },
-
-            {
-                link:'docs',
-            },
-        ]
-    },
-
-    {
-        title: 'about',
-        links:[
-            {
-                link:'stories',
-            },
-
-            {
-                link:'community',
-            },
-
-            {
-                link:'blog',
-            },
-
-            {
-                link:'brand assets',
-            },
-        ]
-    },
-
-    {
-        title: 'features',
-        links:[
-            {
-                link:'overview',
-            },
-
-            {
-                link:'design',
-            },
-
-            {
-                link:'code',
-            },
-
-            {
-                link:'collaborate',
-            },
-            
-            
-            
-            
-        ]
-    },
-
-    {
-        title: 'quick links',
-        links:[
-            {
-                link:'stories',
-            },
-
-            {
-                link:'community',
-            },
-
-            {
-                link:'blog',
-            },
-
-            {
-                link:'brand assets',
-            },
-        ]
-    },
-];
-
-const dataCarousels = [
-    {
-        id: 1,
-        img: '/healthy-foods.webp',
-        data: 'The Best Healthy Foods in 2022',
-        smallDate: 'December 26, 2022',
-        userIcon: '',
-        dateIcon: '',
-        text: '',
-        tags:[
-            {
-                tag: 'food',
-            }
-        ]
-    },
-
-    {
-        id: 2,
-        img: '/winter.webp',
-        data: 'The Best Winter Outfits',
-        smallDate: 'December 26, 2022',
-        userIcon: '',
-        dateIcon: '',
-        text: '',
-        tags:[
-            {
-                tag: 'fashion',
-            }
-        ]
-    },
-
-    {
-        id: 3,
-        img: '/photographers-mistakes.webp',
-        data: 'Beginner Photographer\'s Mistakes',
-        smallDate: 'December 26, 2022',
-        userIcon: '',
-        dateIcon: '',
-        text: '',
-        tags:[
-            {
-                tag: 'fashion',
-            }
-        ]
-    },
-
-    {
-        id: 4,
-        img: '/ideas-anime.webp',
-        data: 'Live Ideas You Might Be Anime',
-        smallDate: 'December 26, 2022',
-        userIcon: '',
-        dateIcon: '',
-        text: '',
-        tags:[
-            {
-                tag: 'culture',
-            },
-
-            {
-                tag: 'stories',
-            }
-        ]
-    },
-
-];
-
-const secondCarousels =[
-    {
-        id: 6,
-        img: '/anime-fashion.webp',
-        data: 'Fashion Trend Now A Days',
-        smallDate: 'December 25, 2022',
-        userIcon: 'fa-solid fa-user',
-        dateIcon: 'fa-solid fa-calendar-days',
-        text: 'demo',
-        tags:[
-            {
-                tag: 'Fashion',
-            },
-
-            {
-                tag: 'Lifestyle',
-            }
-        ]
-    },
-
-    {
-        id: 7,
-        img: '/travel-alone-300x200.webp',
-        data: 'Travelling Alone Is Awesome',
-        smallDate: 'December 26, 2022',
-        userIcon: 'fa-solid fa-user',
-        dateIcon: 'fa-solid fa-calendar-days',
-        text: 'demo',
-        tags:[
-            {
-                tag: 'Stories',
-            },
-
-            {
-                tag: 'Travel',
-            }
-        ]
-    },
-
-    {
-        id: 8,
-        img: '/best-places.webp',
-        data: 'Places For A Road Trip',
-        smallDate: 'December 25, 2022',
-        userIcon: 'fa-solid fa-user',
-        dateIcon: 'fa-solid fa-calendar-days',
-        text: 'demo',
-        tags:[
-            {
-                tag: 'Stories',
-            },
-
-            {
-                tag: 'Travel',
-            }
-        ]
-    },
-
-    {
-        id: 9,
-        img: '/music-love.webp',
-        data: 'Music The Love Of My Life',
-        smallDate: 'December 25, 2022',
-        userIcon: 'fa-solid fa-user',
-        dateIcon: 'fa-solid fa-calendar-days',
-        text: 'demo',
-        tags:[
-            {
-                tag: 'Culture',
-            },
-
-            {
-                tag: 'Lifestyle',
-            }
-        ]
-    },
-
-    {
-        id: 10,
-        img: '/visit-france.webp',
-        data: 'Reasons To Visit France',
-        smallDate: 'December 26, 2022',
-        userIcon: 'fa-solid fa-user',
-        dateIcon: 'fa-solid fa-calendar-days',
-        text: 'demo',
-        tags:[
-            {
-                tag: 'Lifestyle',
-            },
-
-            {
-                tag: 'Travel',
-            }
-        ]
-    },
-
-    {
-        id: 11,
-        img: '/success-story.webp',
-        data: 'The Best Success Stories',
-        smallDate: 'December 25, 2022',
-        userIcon: 'fa-solid fa-user',
-        dateIcon: 'fa-solid fa-calendar-days',
-        text: 'demo',
-        tags:[
-            {
-                tag: 'Culture',
-            },
-        ]
-    },
-
-];
-
-export{dataLinks, dataCarousels, secondCarousels};
