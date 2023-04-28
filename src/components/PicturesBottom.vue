229b788d911d63d558de26031bd93d82674fed1d
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-28 10:47:41 +0200

    Upgraded cards

---
+++ src/components/PicturesBottom.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container-fluid d-flex justify-content-center align-items-center">
+        <div class="card m-2" v-for="img in data.picturesBottom">
+            <img class="image-f" :src="'/image' + img.img" :alt="img.data">
+            <div class="text text-white fw-semibold text-uppercase">{{ img.data }}</div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import { data } from '../data/data';
+export default {
+    name: 'PicturesBottom',
+    data() {
+        return {
+            data,
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .card{
+        width: 300px;
+        height: 200px;
+        background-color: black;
+        position: relative;
+        border: 0px;
+    }
+    .image-f{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        opacity: 0.6;
+        border-radius: 5px;
+
+    }
+    .text{
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 25px;
+    }
+</style>
