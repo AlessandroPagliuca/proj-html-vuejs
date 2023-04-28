8b3c55c5f566664d38e0c3891d5c00b073a2691d
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-28 13:42:22 +0200

    Added plugin fonts

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fonts from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    fonts({
+      google: {
+        families: [
+          'Pacifico:400',
+        ]
+      }
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
